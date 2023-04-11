50c636e864578f7be04612f74871841c8e6a89a0
gracie gyimah <graciegyimah9@gmail.com>
2023-04-11 17:37:42 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = `roller-skate`) {
+    //console.log("You've been razzled!");
+    return (`This Saturday, I want to ${activity}!`)
+  }
+  saturdayFun()
+
+  function mondayWork (werk = `go to the office`){
+   // console.log ("")
+   return (`This Monday, I will ${werk}.`)
+  }
+function wrapAdjective (visualflair = `*`){
+    return function(lala = `special`){
+        return (`You are ${visualflair}${lala}${visualflair}!`)
+    }
+}
+
